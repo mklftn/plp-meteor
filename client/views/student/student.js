64bfc626c98fde29e05469733472dfa841a03179
Mickael Lecoq <m.blacksad@gmail.com>
2015-01-25 11:28:35 +0100

    ajout de la selection de la classe

--- client/views/student/student.js
+++ client/views/student/student.js
@@ -1,7 +1,23 @@
 Template.studentPage.helpers({
-     groups: function () {
-       return this.groups;
+     displayedGroups: function () {
+     	var result = new Array();
+     	for(var i=0; i<this.groups.length; i++){
+     		var item = {
+     			groupName : this.groups[i],
+     			idSchool : this._id,
+     			selected : this.groups[i] === this.selectedGroup
+     		}
+
+     		result.push(item);
+     	}  	
+       return result;
      }
+});
+
+Template.studentPage.events({
+	"click .group-name" : function(e) {
+		Meteor.call("updateSelectedGroup", this.idSchool, this.groupName);
+	}
 });
 
 
@@ -13,5 +29,8 @@ Template.groupCreation.events = {
     		var nomClasse = $(e.target).find('[name=nomClasse]').val();
       			
     		Schools.update({"_id" : this._id}, { "$addToSet": { "groups" : nomClasse } } );
+
+    		Meteor.call("updateSelectedGroup", this._id, nomClasse);
+
   		}
 };
