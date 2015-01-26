ff13f9483d4a72875e04062e45355095d8f7fffc
mklftn <mklftn@gmail.com>
2015-01-26 19:58:30 +0100

    Amelioration creation de la salle

--- client/views/room/room.js
+++ client/views/room/room.js
@@ -1,3 +1,40 @@
+Template.roomPage.helpers({
+     displayedRooms: function () {
+          var result = new Array();
+          for(var i=0; i<this.rooms.length; i++){
+               var item = {
+                    roomName : this.rooms[i].nom,
+                    nbLignes : this.rooms[i].taille.lignes,
+                    nbColonnes : this.rooms[i].taille.colonnes,
+                    idSchool : this._id,
+                    selected : this.rooms[i] === this.selectedRooms
+               }
+              result.push(item);
+          }    
+          return result;
+     },
+     displayedLignes: function () {
+          var result = new Array();
+          for(var i=1; i<10; i++){
+               var item = {
+                    nbLignes : i
+               }
+               result.push(item);
+          }
+          return result;
+     }
+});
+
+Template.roomPage.events({
+     "click .room-item" : function(e) {
+          $('#nbLignes').text(this.nbLignes);
+          Meteor.call("updateSelectedRoom", this.idSchool, this.roomName);
+     },
+     "click .nbRow" : function(e) {
+          alert(this.nbLignes);
+     }
+});
+
 Template.roomCreation.events = {
 	"submit form": function(e) {
 
@@ -5,7 +42,7 @@ Template.roomCreation.events = {
 
 		var nomSalle = $(e.target).find('[name=nomSalle]').val();
 
-		Schools.update({"_id" : this._id}, { "$addToSet": { "classrooms" : {"nom" : nomSalle, "size" : {"width" : 6, "height" : 5}}}} );
+		Schools.update({"_id" : this._id}, { "$addToSet": { "rooms" : {"nom" : nomSalle, "taille" : {"lignes" : 6, "colonnes" : 5}}}} );
 
 	}
 }; 
