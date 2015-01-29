6ecdf8d2ad4f5488ba9983209db8ab5a3667325f
mklftn <mklftn@gmail.com>
2015-01-29 20:31:56 +0100

    Room selection nb colonnes + session

--- client/views/room/room.js
+++ client/views/room/room.js
@@ -1,3 +1,10 @@
+Template.roomPage.rendered = function () {
+     var nbRow = Session.get("roomRow");
+     var nbCol = Session.get("roomCol");
+     $('#lignes').val(nbRow);
+     $('#colonnes').val(nbCol);
+};
+
 Template.roomPage.helpers({
      displayedRooms: function () {
           var result = new Array();
@@ -22,6 +29,16 @@ Template.roomPage.helpers({
                result.push(item);
           }
           return result;
+     },
+     displayedColonnes: function () {
+          var result = new Array();
+          for(var i=1; i<10; i++){
+               var item = {
+                    nbColonnes : i
+               }
+               result.push(item);
+          }
+          return result;
      }
 });
 
@@ -29,6 +46,10 @@ Template.roomPage.events({
      "click .room-item" : function(e) {
           $('#nbLignes').text(this.nbLignes);
           $('#lignes').val(this.nbLignes);
+          $('#colonnes').val(this.nbColonnes);
+          Session.set("roomName", this.roomName);
+          Session.set("roomRow", this.nbLignes);
+          Session.set("roomCol", this.nbColonnes);
           Meteor.call("updateSelectedRoom", this.idSchool, this.roomName);
      },
      "change #lignes" : function(e){
@@ -36,7 +57,16 @@ Template.roomPage.events({
           var nomSalle = $("#roomActive").text();
           var nbRow = $("#lignes option:selected").val();
           var idSchool = this._id;
+          Session.set("roomRow", nbRow);
           Meteor.call("updateRowRoom", idSchool, nomSalle, nbRow);
+     },
+     "change #colonnes" : function(e){
+          e.preventDefault();
+          var nomSalle = $("#roomActive").text();
+          var nbCol = $("#colonnes option:selected").val();
+          var idSchool = this._id;
+          Session.set("roomCol", nbCol);
+          Meteor.call("updateColRoom", idSchool, nomSalle, nbCol);
      }
 });
 
