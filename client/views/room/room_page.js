84ac54cd492d2d1c73d72013f69da2c45a37b9f3
mklftn <mklftn@gmail.com>
2015-02-22 11:57:03 +0100

    Reactive templating pour l'affichage de la room

--- client/views/room/room_page.js
+++ client/views/room/room_page.js
@@ -16,7 +16,7 @@ Template.roomPage.helpers({
           var result = new Array();
 
           var nbLigne = 1;
-           for(var i=0; i<this.rooms.length; i++){
+          for(var i=0; i<this.rooms.length; i++){
                if(this.rooms[i].nom === this.selectedRoom){
                     nbLigne = this.rooms[i].taille.lignes;
                }
@@ -36,7 +36,7 @@ Template.roomPage.helpers({
           var result = new Array();
 
           var nbColonne = 1;
-           for(var i=0; i<this.rooms.length; i++){
+          for(var i=0; i<this.rooms.length; i++){
                if(this.rooms[i].nom === this.selectedRoom){
                     nbColonne = this.rooms[i].taille.colonnes;
                }
@@ -62,14 +62,73 @@ Template.roomPage.events({
           e.preventDefault();
           var nomSalle = $("#roomActive").text();
           var nbRow = $("#lignes option:selected").val();
-          var idSchool = this._id;
-          Meteor.call("updateRowRoom", idSchool, nomSalle, nbRow);
+          var nbCol = $("#colonnes option:selected").val();
+          updateRowRoomAndSeat(this, nomSalle, nbRow, nbCol);
      },
      "change #colonnes" : function(e){
           e.preventDefault();
           var nomSalle = $("#roomActive").text();
+          var nbRow = $("#lignes option:selected").val();
           var nbCol = $("#colonnes option:selected").val();
-          var idSchool = this._id;
-          Meteor.call("updateColRoom", idSchool, nomSalle, nbCol);
+          updateColRoomAndSeat(this, nomSalle, nbRow, nbCol);
      }
 });
+
+function updateRowRoomAndSeat (school, nomSalle, nbRow, nbCol) {
+         // On modifie le nombre de lignes en bdd
+         Meteor.call("updateRowRoom", school._id, nomSalle, nbRow);
+         // On récupère le nombre de lignes stocké en base pour supprimer ou ajouter les places nécessaires
+         for(var i=0; i<school.rooms.length; i++){
+          if(school.rooms[i].nom === nomSalle){
+              nbLigne = school.rooms[i].taille.lignes;
+           // Si le nombre de lignes en base est plus élevé, on supprime les places
+           if (nbLigne > nbRow) {
+               for(var j=nbLigne; j>nbRow; j--){
+                    for (var k=0; k<nbCol; k++){
+                         var seat = (parseInt(j,10)*10) + (parseInt(k,10) + 1);
+                         Meteor.call('supprimerSeat', school._id, nomSalle, seat);
+                    }
+               }
+          }
+          // Si le nombre de lignes en base est plus faible, on ajoute les places
+          if (nbLigne < nbRow) {
+               for(var j=nbRow; j>nbLigne; j--){
+                    for (var k=0; k<nbCol; k++){
+                         var seat = (parseInt(j,10)*10) + (parseInt(k,10) + 1);
+                         Meteor.call('ajouterSeat', school._id, nomSalle, seat);
+                    }
+               }
+          }
+     }
+}
+}
+
+function updateColRoomAndSeat (school, nomSalle, nbRow, nbCol) {
+         // On modifie le nombre de colonnes en bdd
+         Meteor.call("updateColRoom", school._id, nomSalle, nbCol);
+         // On récupère le nombre de colonnes stocké en base pour supprimer ou ajouter les places nécessaires
+         for(var i=0; i<school.rooms.length; i++){
+          if(school.rooms[i].nom === nomSalle){
+              nbColonne = school.rooms[i].taille.colonnes;
+           // Si le nombre de colonnes en base est plus élevé, on supprime les places
+           if (nbColonne > nbCol) {
+               for(var j=nbColonne; j>nbCol; j--){
+                    for (var k=nbRow; k>0; k--){
+                         var seat = (parseInt(k,10)*10) + parseInt(j,10);
+                         Meteor.call('supprimerSeat', school._id, nomSalle, seat);
+                    }
+               }
+          }
+          // Si le nombre de colonnes en base est plus faible, on ajoute les places
+          if (nbColonne < nbCol) {
+               for(var j=nbCol; j>nbColonne; j--){
+                    for (var k=nbRow; k>0; k--){
+                         var seat = (parseInt(k,10) * 10) + parseInt(j,10);
+                         Meteor.call('ajouterSeat', school._id, nomSalle, seat);
+                    }
+               }
+          }
+     }
+}
+alert(result);
+}
---
+++ client/views/room/room_vision.js
@@ -0,0 +1,66 @@
+Template.roomVision.events({
+// Modifie suite a un click le statut seatactivity d'une place : active or inactive  
+"click .cel" : function(e) {
+     var idSchool = this._id;
+     var nomSalle = this.selectedRoom;
+     var seatPosition = $(e.target).attr("data-numeroplace");
+     if($(e.target).attr("data-seatactivity") == "active"){
+          Meteor.call("supprimerSeat", idSchool, nomSalle, seatPosition);
+     } else{
+          Meteor.call("ajouterSeat", idSchool, nomSalle, seatPosition);
+     };
+}
+});
+
+Template.roomVision.helpers({
+     roomView: function () {
+          var nbColonne = 1;
+          var nbLigne = 1;
+          var position = new Array();
+          var id = this._id;
+          var selectedR = this.selectedRoom;
+          for(var i=0; i<this.rooms.length; i++){
+               if(this.rooms[i].nom === this.selectedRoom){
+                    nbColonne = this.rooms[i].taille.colonnes;
+                    nbLigne = this.rooms[i].taille.lignes;
+                    positions = this.rooms[i].positions;
+               }
+          }
+
+          var intLignes = new Array();
+          for(var i=0; i<nbLigne; i++){
+               var j = nbLigne - i;
+               var intColonnes = new Array();
+               for(var k=0; k<nbColonne; k++){
+                    var l = k +1;
+                    var place = (j * 10) + l;
+                    var seatActivity = new String();
+                    if(jQuery.inArray(place, positions)!==-1){
+                         seatAct = "active";
+                    } else{
+                         seatAct = "inactive";
+                    }
+
+                    var col = {
+                         colonne: k,
+                         seat: place,
+                         seatActivity: seatAct,
+                         _id: id,
+                         selectedRoom: selectedR
+
+                    }
+                    intColonnes.push(col);
+               }
+               var lig = {
+                    ligne: j,
+                    colonnes: intColonnes
+               }
+               intLignes.push(lig);
+          }
+
+          var roomInfo = {
+               lignes: intLignes
+          }
+          return roomInfo;
+     }
+});
