57e6fb609a675519b2c6c23662eb259ae72ff0da
mklftn <mklftn@gmail.com>
2015-02-27 23:05:01 +0100

    Modification du nom de la room

--- client/views/room/menu/room_select.js
+++ client/views/room/menu/room_select.js
@@ -15,6 +15,38 @@ Template.roomSelect.helpers({
 
 Template.roomSelect.events({
      "click .room-item" : function(e) {
+          e.defaultPrevented;
           Meteor.call("updateSelectedRoom", this.idSchool, this.roomName);
-     }
+          return false;
+     },
+     "click #modifRoomName" : function(e) {
+          e.defaultPrevented;
+          $("#selectRoom").addClass("hide");
+          $("#changeNameRoom").removeClass("hide");
+          $("#newName").focus();
+          return false;
+     },
+     "click #validNewRoomName" : function(e) {
+          e.defaultPrevented;
+          validerRoomName(this._id);
+          return false;
+     },
+     "keypress #newName" : function(e) {
+          e.defaultPrevented;
+          if(e.which == 13) {
+          validerRoomName(this._id);
+          return false;
+  }
+}
 });
+
+function validerRoomName(id){
+          var oldName = $("#newName").attr('data-oldName');
+          var newName = $("#newName").val();
+          if(oldName != newName){
+               Meteor.call("updateRoomName", id, oldName, newName);
+               Meteor.call("updateSelectedRoom", id, newName);
+          }
+          $("#changeNameRoom").addClass("hide");
+          $("#selectRoom").removeClass("hide");
+}
