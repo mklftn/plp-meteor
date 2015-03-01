dd6c6d23e73ff6f06968ed378ece6d4507a8d460
mklftn <mklftn@gmail.com>
2015-03-01 11:09:30 +0100

    Correction bug affichage nom schoolactive dans delete room

--- client/views/gestion/delete_room.js
+++ client/views/gestion/delete_room.js
@@ -1,11 +1,11 @@
 Template.deleteRoom.helpers({
-     displayedRooms: function (school) {
+     displayedRooms: function (ecole) {
           var result = new Array();
-          for(var i=0; i<school.rooms.length; i++){
+          for(var i=0; i<ecole.rooms.length; i++){
                var item = {
-                    roomName : school.rooms[i].nom,
-                    idSchool : school._id,
-                    selected : school.rooms[i].nom === school.selectedRoom
+                    roomName : ecole.rooms[i].nom,
+                    idecole : ecole._id,
+                    selected : ecole.rooms[i].nom === ecole.selectedRoom
                }
                result.push(item);
           }    
@@ -18,7 +18,7 @@ Template.deleteRoom.events({
           e.defaultPrevented;
           $("#button_delete").removeClass("hide");
           $("#confirmDeleteRoom").addClass("hide");
-          Meteor.call("updateSelectedRoom", this.idSchool, this.roomName);
+          Meteor.call("updateSelectedRoom", this.idecole, this.roomName);
           $('[data-toggle="dropdown"]').parent().removeClass('open');
           return false;
      },
@@ -31,20 +31,20 @@ Template.deleteRoom.events({
      },
      "click #validSupp" : function(e) {
           e.defaultPrevented;
-          var salleToSup = this.school.selectedRoom;
-          var idSchool = this.school._id;     
-          if(this.school.rooms.length > 1){
+          var salleToSup = this.ecole.selectedRoom;
+          var idecole = this.ecole._id;     
+          if(this.ecole.rooms.length > 1){
                var newSelectedRoom = new String();
-               if(this.school.rooms[0].nom != salleToSup){
-                    newSelectedRoom = this.school.rooms[0].nom;
+               if(this.ecole.rooms[0].nom != salleToSup){
+                    newSelectedRoom = this.ecole.rooms[0].nom;
                } else {
-                    newSelectedRoom = this.school.rooms[1].nom;
+                    newSelectedRoom = this.ecole.rooms[1].nom;
                }
-               Meteor.call("updateSelectedRoom", idSchool, newSelectedRoom);
+               Meteor.call("updateSelectedRoom", idecole, newSelectedRoom);
           } else{
-               Meteor.call("updateSelectedRoom", idSchool, "");
+               Meteor.call("updateSelectedRoom", idecole, "");
           }
-          Meteor.call("deleteRoom", idSchool, salleToSup);
+          Meteor.call("deleteRoom", idecole, salleToSup);
           $("#selectDeleteRoom").removeClass("hide");
           $("#button_delete").removeClass("hide");
           $("#confirmDeleteRoom").addClass("hide");
